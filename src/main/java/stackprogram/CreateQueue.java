b9eb385b496c8fbbe700fc6add80faf0165e7284
Nrupesh Mhatre <sanjanamhatre555@gmail.com>
2022-04-06 14:59:56 +0530

    Create Queue added

---
+++ src/main/java/stackprogram/CreateQueue.java
@@ -0,0 +1,52 @@
+package stackprogram;
+
+public class CreateQueue {
+	public Node head;
+    public Node tail;
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    
+    public void append(int datas){
+        Node newNode = new Node(datas);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+   
+    public void display() {
+        Node current = head;
+        if(head == null) {
+            System.out.println("List is empty");
+        }
+        while (current != null) {
+            System.out.println(current.data +" ");
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+    	CreateQueue queue = new CreateQueue();
+        queue.append(70);
+        queue.append(30);
+        queue.append(56);
+
+        queue.display();
+    }
+
+
+}
+
